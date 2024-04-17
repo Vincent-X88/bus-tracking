5a8f95d9bdbac085024f027767bcc938366c370e
Xolani Mavuso <vxolani777@gmail.com>
2024-04-17 17:52:43 +0200

    Update deletePassengerAccount.php

--- Bus tracking/modified_php_files/deletePassengerAccount.php
+++ Bus tracking/modified_php_files/deletePassengerAccount.php
@@ -4,7 +4,7 @@ include '../connection.php';
 $email = $_POST['email'];
 $user_password = md5($_POST['user_password']);
 
-$updateQuery = "UPDATE users_table SET is_active = 0 WHERE email = '".$email."' AND user_password = '".$user_password."' AND is_active = 1";
+$updateQuer = "UPDATE users_table SET is_active = 0 WHERE email = '".$email."' AND user_password = '".$user_password."' AND is_active = 1";
 
 $queryUpdateResponse = $connectNow->query($updateQuery);
 
