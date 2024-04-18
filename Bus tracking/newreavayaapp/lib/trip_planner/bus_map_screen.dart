b2fd6c635a59d2644ae002df4da517da9b78f986
Xolani Mavuso <vxolani777@gmail.com>
2024-04-18 17:14:09 +0200

    Update bus_map_screen.dart

--- Bus tracking/newreavayaapp/lib/trip_planner/bus_map_screen.dart
+++ Bus tracking/newreavayaapp/lib/trip_planner/bus_map_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
-class BusET {
+class BusETA {
   late final DateTime estimatedArrivalTime;
   late Duration remainingTime;
 
