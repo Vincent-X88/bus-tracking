e80a25a5d937d478c4f403da1ef25003a8e4fef5
Xolani Mavuso <vxolani777@gmail.com>
2024-04-19 15:35:22 +0200

    Update passenger_feedback_page.dart

--- Bus tracking/newreavayaapp/lib/passenger_feedback/passenger_feedback_page.dart
+++ Bus tracking/newreavayaapp/lib/passenger_feedback/passenger_feedback_page.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:fluttertoast/fluttertoast.dart';
-impor 'package:get/get.dart';
+import 'package:get/get.dart';
 
 import '../api_connection/api_connection.dart';
 import '../authentication/edit_account_screen.dart';
